7f6457503292b352f3a230f6a191cd9ecc946bde
sdburt <seandburt@gmail.com>
2023-01-14 15:20:44 -0800

    more content security changes

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self' sdburt.com *.sdburt.com;
+  default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app vitals.vercel-insights.com;
   child-src sdburt.com *.sdburt.com sdburt-com.vercel.app *.sdburt-com.vercel.app;
   style-src 'self' 'unsafe-inline';
