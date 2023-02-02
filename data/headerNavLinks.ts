9391c53886d2c2d33c48ee1d0a593a9622a320c2
sdburt <seandburt@gmail.com>
2023-02-01 18:28:52 -0800

    remove dashboard and update about page to include top 10 spotify songs

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,5 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/dashboard', title: 'Dashboard' },
   { href: '/projects', title: 'Projects' },
   { href: '/blog', title: 'Blog' },
   { href: '/about', title: 'About' },
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,6 @@
 import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { getFileBySlug } from '@/lib/mdx'
+import TopTracks from '@/components/spotify/TopTracks'
 
 const DEFAULT_LAYOUT = 'AuthorLayout'
 
@@ -12,10 +13,23 @@ export default function About({ authorDetails }) {
   const { mdxSource, frontMatter } = authorDetails
 
   return (
-    <MDXLayoutRenderer
-      layout={frontMatter.layout || DEFAULT_LAYOUT}
-      mdxSource={mdxSource}
-      frontMatter={frontMatter}
-    />
+    <>
+      <MDXLayoutRenderer
+        layout={frontMatter.layout || DEFAULT_LAYOUT}
+        mdxSource={mdxSource}
+        frontMatter={frontMatter}
+      />
+      <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+        <div className="flex flex-col items-start justify-center xl:col-span-2 xl:col-start-2">
+          <h2 className="mb-4 mt-4 text-3xl font-bold tracking-tight text-black dark:text-white">
+            Top Tracks
+          </h2>
+          <p className="mb-4 text-gray-600 dark:text-gray-400">
+            Curious what I'm currently listening to? Here's my top tracks on Spotify!
+          </p>
+          <TopTracks />
+        </div>
+      </div>
+    </>
   )
 }
